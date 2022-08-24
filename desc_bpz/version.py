a899379a6a163cc90d4c5d9b5ac2571eb06eb850
sschmidt23 <sschmidt@physics.ucdavis.edu>
2022-08-24 14:22:02 -0700

    add version file, update license, email in setup

---
+++ desc_bpz/version.py
@@ -0,0 +1,2 @@
+# just set version manually for now
+__version__ = "0.1"
--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_namespace_packages
 packages = find_namespace_packages()
 setup(
     name="desc_bpz",
-    version="0.0.1",
+    version="0.1",
     author="Noel Benitez, Dan Coe, Will Hartley,"
         "Sam Schmidt, LSST DESC PZWG",
-    author_email="fake@fake.edu",
+    author_email="samschmidt@ucdavis.edu",
     packages=packages,
     package_data={
         "": ["*.h5", "*.yaml", "*.sed", "*.res",
@@ -18,13 +18,13 @@ setup(
         "scripts": ["*.columns, *.pars"]
     },
     include_package_data=True,
-    license="BSD 3-Clause License",
+    license="MIT License",
     description="Python3 version of BPZ used in DESC",
     url="https://github.com/LSSTDESC/DESC_BPZ",
     long_description=open("README.md").read(),
     classifiers=[
         "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: BSD 3-Clause",
+        "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Operating System :: OS Independent",
